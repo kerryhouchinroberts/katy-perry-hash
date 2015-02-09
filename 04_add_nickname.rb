30c8915b139aad8e56254829cd2475b3b9435e4f
Kerry Roberts <kerry.h.roberts@gmail.com>
2015-02-09 10:07:03 -0700

    Complete first 6 exercises.

--- 04_add_nickname.rb
+++ 04_add_nickname.rb
@@ -1,3 +1,7 @@
 require_relative "person"
 
 # Add a nickname to the KATY_PERRY hash and print it out. Don't edit the person.rb file!
+nickname = {:nickname => "KPerr"}
+KATY_PERRY.merge!(nickname)
+
+puts KATY_PERRY[:nickname]
